3fee7f54431e1ab475b2dd919399bf6d5b85aadf
bit1002lst <bit_lst@163.com>
2024-01-27 15:06:47 +0800

    update

--- char_generate/draw_char.py
+++ char_generate/draw_char.py
@@ -12,7 +12,11 @@ import re
 import pdb
 import math
 import uuid
+import pdb
 
+
+# bbox定义
+# [x1,y1,x2,y2]
 def fonttoUnicode(oneStr):
     t=oneStr
     if  t[:3] == 'uni':t=t.replace('uni','\\u') 
@@ -82,8 +86,8 @@ def draw_chinese_char(input_char, src_img, font, pos='center', font_color=None):
         font_size = 100
         ft = ImageFont.truetype(font=font, size=font_size)
         word_width, word_height = draw.textsize(input_char, font=ft)
-        lt_pos = ((128-word_width)/2, (128-word_height)/2)
-        rb_pos = (lt_pos[0] + word_width, lt_pos[1] + word_height)
+        lt_pos = [(128-word_width)/2, (128-word_height)/2]
+        rb_pos = [lt_pos[0] + word_width, lt_pos[1] + word_height]
     if font_color is None:
         font_color = (random.randint(0,50), random.randint(0,50), random.randint(0,50))
     draw.text(lt_pos, input_char, fill = font_color, font=ft)
@@ -92,7 +96,7 @@ def draw_chinese_char(input_char, src_img, font, pos='center', font_color=None):
     im_gt = np.array(im_gt)
     kernel_size = (random.randint(0,7)*2+1, random.randint(0,7)*2+1)
     cv2.GaussianBlur(im_tmp, kernel_size, 0, 1)
-    return im_tmp, im_gt, [lt_pos, rb_pos]
+    return im_tmp, im_gt, [lt_pos[::-1], rb_pos[::-1]]
 
 def draw_unicode(input_range):
     start_index = int(input_range[0], 16)
@@ -139,30 +143,101 @@ def example():
 def draw_char(char, bg_img, font):
     pass
 
-def img_split(src_img, src_gt):
-    img_size = src_img.shape[:2]
+def isin_bbox(bbox_1, bbox_2, iou_thres=0.8):
+    # is bbox 1 in bbox 2
+    if isinstance(bbox_1[0], list):
+        bbox_1 = bbox_1[0] + bbox_1[1]
+    if isinstance(bbox_2[0], list):
+        bbox_2 = bbox_2[0] + bbox_2[1]
     
-    pass
+    x1 = max(bbox_1[0], bbox_2[0])
+    y1 = max(bbox_1[1], bbox_2[1])
+    x2 = min(bbox_1[2], bbox_2[2])
+    y2 = min(bbox_1[3], bbox_2[3])
+    
+    intersection = (x2 - x1) * (y2 - y1)
+    area_box1 = (bbox_1[2] - bbox_1[0]) * (bbox_1[3] - bbox_1[1])
+    area_box2 = (bbox_2[2] - bbox_2[0]) * (bbox_2[3] - bbox_2[1])
+    union = area_box1 + area_box2 - intersection
+    
+    bbox_iou_1 = intersection / area_box1 
+    if bbox_iou_1 >= iou_thres:
+        return True
+    else:
+        return False
+
+
+def img_split(src_img, src_gt):
+    img_h, img_w = src_img.shape[:2]
+    out_img_list = []
+    out_gt_list = []
+    if img_h <= img_w:
+        for i in range(int(img_w / img_h) + 1):
+            img_bbox = [[0,i*img_h],[img_h,(i+1)*img_h]]
+            if (i+1)*img_h > img_w:
+                img_bbox = [[0,img_w-img_h], [img_h,img_w]]
+            tmp_img = src_img[img_bbox[0][0]:img_bbox[1][0],
+                              img_bbox[0][1]:img_bbox[1][1]]
+            tmp_gt = []
+            for gt_item in src_gt:
+                if isin_bbox(gt_item, img_bbox):
+                    bbox_item = [gt_item[0]-img_bbox[0][0], 
+                                 gt_item[1]-img_bbox[0][1],
+                                 gt_item[2]-img_bbox[0][0], 
+                                 gt_item[3]-img_bbox[0][1]]
+                    tmp_gt.append(bbox_item)
+            out_img_list.append(tmp_img)
+            out_gt_list.append(tmp_gt)
+    if img_h > img_w:
+        for i in range(int(img_h / img_w) + 1):
+            img_bbox = [[i*img_w,0], [(i+1)*img_w,img_w]]
+            if (i+1)*img_w > img_h:
+                img_bbox = [[img_h-img_w,0], [img_h,img_w]]
+            tmp_img = src_img[img_bbox[0][0]:img_bbox[1][0],
+                              img_bbox[0][1]:img_bbox[1][1]]
+            tmp_gt = []
+            for gt_item in src_gt:
+                if isin_bbox(gt_item, img_bbox):
+                    bbox_item = [gt_item[0]-img_bbox[0][0], 
+                                 gt_item[1]-img_bbox[0][1],
+                                 gt_item[2]-img_bbox[0][0], 
+                                 gt_item[3]-img_bbox[0][1]]
+                    tmp_gt.append(bbox_item)
+            out_img_list.append(tmp_img)
+            out_gt_list.append(tmp_gt)
+    return out_img_list, out_gt_list
+
+def np_imread(img_file):
+    cv_img = cv2.imdecode(np.fromfile(img_file, dtype=np.uint8), -1)
+    # cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
+    return cv_img
+
+def show_bbox(src_img,char_list):
+    for tmp_gt in char_list:
+        cv2.rectangle(src_img, (int(tmp_gt[1]), int(tmp_gt[0])),
+                                    (int(tmp_gt[3]), int(tmp_gt[2])), (0,0,255), 3)
+    src_img = cv2.resize(src_img, (512,512))
+    cv2.imshow('tmp', src_img)
+    cv2.waitKey()
 
 def main():
-    font_files = glob.glob('../../fonts/*.ttf') + glob.glob('../../fonts/*.TTF')
+    font_files = glob.glob('..\\..\\fonts\\*.ttf') + glob.glob('..\\..\\fonts\\*.TTF')
     
     font_file = random.choice(font_files)
-    bg_files = glob.glob('../char_binary/img_bgs/*.png')
+    bg_files = glob.glob('..\\char_binary\\img_bgs\\*.png')
     bg_file = random.choice(bg_files)
-    bg_img = cv2.imread(bg_file)
-    # char_img, gt_img, char_pos = draw_chinese_char('我', bg_img, font_file)
-    # cv2.imshow('src', char_img)
-    # cv2.imshow('gt', gt_img)
-    # cv2.waitKey()
+    bg_img = np_imread(bg_file)
     sentence_img, char_list = draw_sentence('混顿饭嘎口红放入奴隶撒娇更加开放而阿奴给你看路人粉结果撒额的按分而看就啊林父爱努蒂萨妇女节的萨夫家的沙发那u内容', bg_img, font_file)
-    img_list, gt_list = img_split(sentence_img, char_list)
+    
     out_name = str(uuid.uuid1())
     out_img_file = os.path.join('', out_name+'.png')
     out_gt_file = os.path.join('', out_name+'.txt')
-    cv2.imwrite()
-
     
+    cv2.imwrite(out_img_file, sentence_img)
+    img_list, gt_list = img_split(sentence_img, char_list)
+
+    for img_index, img_item in enumerate(img_list):
+        show_bbox(img_item, gt_list[img_index])
 
 def draw_sentence(input_str, bg_img, font_file):
     char_pos_list = []
@@ -190,13 +265,14 @@ def draw_sentence(input_str, bg_img, font_file):
                              start_pos[1]: start_pos[1]+128]
             
             char_img, _, char_bbox = draw_chinese_char(char_val, char_bg, font_file, pos='random', font_color=font_color)
+            # show_bbox(char_img, [char_bbox[0] + char_bbox[1]])
+
             bg_img[start_pos[0]: start_pos[0]+128,
                    start_pos[1]: start_pos[1]+128] = char_img
             char_pos = [start_pos[0] + char_bbox[0][0],
                         start_pos[1] + char_bbox[0][1],
                         start_pos[0] + char_bbox[1][0],
                         start_pos[1] + char_bbox[1][1]]
-            
             char_pos_list.append(char_pos)
     return bg_img, char_pos_list
 
